96e1fce7fe3c94b7208684b13f00dab7e5a54273
waqas4346 <waqas4346@gmail.com>
2024-04-30 04:19:52 +0500

    added changes

--- app/controllers/api/partners_controller.rb
+++ app/controllers/api/partners_controller.rb
@@ -1,7 +1,12 @@
 class Api::PartnersController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:index, :show] #
+  before_action :allow_iframe
   def index
     @partners = Partner.all
     render json: @partners
+  end
+
+  def allow_iframe
+    response.headers['Content-Security-Policy'] = 'frame-ancestors none;'
   end
 end
