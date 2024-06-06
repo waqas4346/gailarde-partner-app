e2f9154de1bea6438601ed1e12d2837e3c241362
waqas4346 <waqas4346@gmail.com>
2024-06-06 15:36:25 +0500

    added changes regarding cache with redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,10 +75,10 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
+  config.cache_store = :redis_cache_store, { 
+    url: ENV['REDISCLOUD_URL'] || 'redis://localhost:6379/0', 
+    namespace: 'my_app_cache' 
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
