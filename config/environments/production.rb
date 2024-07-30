1c2e06ebf2c384d532564a5e1bad57d037a9a70a
waqas4346 <waqas4346@gmail.com>
2024-07-30 16:17:26 +0500

    added changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "gailarde_dashboard_production"
+  config.active_job.queue_adapter = :sidekiq
+
 
 
   config.action_controller.perform_caching = true
